2be4b078b5262d7e2331a015ca49b91010bd1a44
Hp <adkhamjon.abd@gmail.com>
2023-04-17 15:05:10 +0200

    Completes the task

--- src/main/java/com/epam/mjc/MethodParser.java
+++ src/main/java/com/epam/mjc/MethodParser.java
@@ -1,5 +1,8 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MethodParser {
 
     /**
@@ -20,6 +23,65 @@ public class MethodParser {
      * @return {@link MethodSignature} object filled with parsed values from source string
      */
     public MethodSignature parseFunction(String signatureString) {
-        throw new UnsupportedOperationException("You should implement this method.");
+
+        String accessModifier = null;
+        String returnType = null;
+        String methodName = null;
+        List<MethodSignature.Argument> args = new ArrayList<>();
+
+        String signature = signatureString.substring(0, signatureString.indexOf("("));
+        String methodArgs = signatureString.substring(signatureString.indexOf("(") + 1, signatureString.indexOf(")"));
+        String[] methodMembers = signature.split(" ");
+
+
+
+        String[] arguments = methodArgs.split(", ");
+
+        System.out.println(methodMembers.length);
+        if (methodMembers.length == 3){
+            System.out.println("");
+            String first = methodMembers[0];
+            String second = methodMembers[1];
+            System.out.println("Hello");
+            if (first.equals("public") || first.equals("private")){
+                accessModifier = first;
+            }
+            if (second.equals("void")){
+                returnType = second;
+            }else {
+                returnType = second;
+            }
+            methodName = methodMembers[2];
+            System.out.println(methodName);
+
+
+        } else if(methodMembers.length == 2){
+            String first = methodMembers[0];
+            String second = methodMembers[1];
+            if (first.equals("void")){
+                returnType = first;
+            }else {
+                returnType = first;
+            }
+            methodName = second;
+        }
+        if (!methodArgs.isEmpty()){
+            /*
+             * Example int first, int second;
+             * methodName = add;
+             * arguments = array of method arguments split by comma.
+             * */
+            for (String argument : arguments){
+                String argumentType = argument.substring(0, argument.indexOf(" "));
+                String argumentName = argument.substring(argument.indexOf(" ") + 1, argument.length());
+                MethodSignature.Argument arg = new MethodSignature.Argument(argumentType, argumentName);
+                args.add(arg);
+            }
+        }
+        MethodSignature ms = new MethodSignature(methodName, args);
+        ms.setAccessModifier(accessModifier);
+        ms.setReturnType(returnType);
+        return ms;
     }
+
 }
